9bb72e3d28ef28f231c9760f38ac91dacbf55037
Filip <becaphilippe@gmail.com>
2021-05-19 16:30:28 +0200

    deleted all samples approach as it will be unneeded

--- src/allSamplesApproach/allSamplesApproachComputables/fullify.m
+++
@@ -1,9 +0,0 @@
-function varargout = fullify(f, varargin)
-
-%FULLIFY makes a function output dense matrices.
-[varargout{1:nargout}] = f(varargin{:});
-for i=1:numel(varargout)
-varargout{i} = full(varargout{i});
-end
-
-end

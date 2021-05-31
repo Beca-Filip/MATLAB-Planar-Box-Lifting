d05e7f6ab7fc1d5280b15c746cc5070d105974f8
Filip <becaphilippe@gmail.com>
2021-05-31 18:08:40 +0200

    removed the 3dof COP function and introduced 6dof

--- src/Model/COP_6DOF_Matrix.m
+++ src/Model/COP_6DOF_Matrix.m
@@ -1,13 +1,13 @@
-function COP = COP_3DOF_Matrix(q,dq,ddq,EW,modelParam)
-%COP_3DOF_MATRIX uses the Inverse Dynamics Model in Dyn_3DOF to calculate
+function COP = COP_6DOF_Matrix(q,dq,ddq,EW,modelParam)
+%COP_6DOF_MATRIX uses the Inverse Dynamics Model in Dyn_6DOF to calculate
 %the position of the CoP.
 %
-%   COP = COP_3DOF_MATRIX(q,dq,ddq,EW,modelParam) returns a (3 x Number of
+%   COP = COP_6DOF_MATRIX(q,dq,ddq,modelParam,EW) returns a (3 x Number of
 %   samples) matrix of COP cartesian positions in the model base frame.
 
 % Calculate the wrenches at the base of the robot with the inverse dynamics
 % model
-[~, EN] = Dyn_3DOF(q,dq,ddq,EW,modelParam);
+[~, EN] = Dyn_6DOF(q,dq,ddq,EW,modelParam);
 
 % Get the position of the COP
 % COPx = Mz / Fy

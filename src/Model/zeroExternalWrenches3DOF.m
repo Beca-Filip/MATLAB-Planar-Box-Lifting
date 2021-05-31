4e9a90a41fb48b251c8ff2062a8d0892e059a8e9
Filip <becaphilippe@gmail.com>
2021-05-31 16:03:48 +0200

    deleted squatting related files

--- src/Model/zeroExternalWrenches3DOF.m
+++
@@ -1,9 +0,0 @@
-function ZEW = zeroExternalWrenches3DOF(N)
-%ZEROEXTERNALWRENCHES3DOF returns zero external wrenches variable of size N
-    ZEW.FX = zeros(3, N);
-    ZEW.FY = zeros(3, N);
-    ZEW.FZ = zeros(3, N);
-    ZEW.CX = zeros(3, N);
-    ZEW.CY = zeros(3, N);
-    ZEW.CZ = zeros(3, N);
-end

516e40ff1e617ea7885ef1140e7e8d3ee28f2b1d
Filip <becaphilippe@gmail.com>
2021-06-08 15:35:58 +0200

    Added appendix to plot COP across time, added appendices to match the cost and constraint resolutions

---
+++ src/Optimization/Motion_Optimization_Appendix_2_ItpResolutionCost.m
@@ -0,0 +1,99 @@
+%% Flags
+AppendixFlags.PlotCOP = true;
+
+%% Get the time vector with the number of points desired and re-generate trajectories
+
+% Time
+Time = linspace(t0, tf, itpParam.ItpResolutionCost);
+
+% Extract knots of individual joints
+q_knots_star = cell(1, NJ);
+for ii = 1 : NJ
+    q_knots_star{ii} = x_star(1 + (ii-1) * Ncp : ii * Ncp);
+end
+
+% Get spline coeffs
+polycoeffs_star = cell(1, NJ);
+for ii = 1 : NJ
+    polycoeffs_star{ii}  = splineInterpolation2(Tknots, q_knots_star{ii}, ItpOrder, BndCnd);
+end
+
+% Get trajectories, velocities, accelerations, and jerks and separate them
+q_tva = cell(1, NJ);
+cell_q_star = cell(1, NJ);
+cell_dq_star = cell(1, NJ);
+cell_ddq_star = cell(1, NJ);
+cell_dddq_star = cell(1, NJ);
+
+for ii = 1 : NJ
+    q_tva{ii} = splineCoefToTrajectory(Tknots, polycoeffs_star{ii}, Time, 3);
+    cell_q_star{ii} = q_tva{ii}(1, :).'; % Transpose for later
+    cell_dq_star{ii} = q_tva{ii}(2, :).'; % Transpose for later
+    cell_ddq_star{ii} = q_tva{ii}(3, :).'; % Transpose for later 
+    cell_dddq_star{ii} = q_tva{ii}(4, :).'; % Transpose for later    
+end
+
+% Merge trajectories velocities and accelerations into one matrix
+q_star = [cell_q_star{:}].'; % Merge and transpose so rows correspond to single joint trajectories
+dq_star = [cell_dq_star{:}].'; % Merge and transpose so rows correspond to single joint velocities
+ddq_star = [cell_ddq_star{:}].'; % Merge and transpose so rows correspond to single joint accelerations
+dddq_star = [cell_dddq_star{:}].'; % Merge and transpose so rows correspond to single joint jerks
+
+%% Get human trajectories and compate with optimal ones
+
+% Human knots
+q_knots_human = q(:, itpParam.KnotIndices);
+x_human = q_knots_human.';
+x_human = x_human(:).';
+
+% Get spline coeffs
+polycoeffs_human = cell(1, NJ);
+for ii = 1 : NJ
+    polycoeffs_human{ii}  = splineInterpolation2(Tknots, q_knots_human(ii, :), ItpOrder, BndCnd);
+end
+
+% Get trajectories, velocities, accelerations, and jerks and separate them
+q_tva = cell(1, NJ);
+cell_q_human = cell(1, NJ);
+cell_dq_human = cell(1, NJ);
+cell_ddq_human = cell(1, NJ);
+cell_dddq_human = cell(1, NJ);
+
+for ii = 1 : NJ
+    q_tva{ii} = splineCoefToTrajectory(Tknots, polycoeffs_human{ii}, Time, 3);
+    cell_q_human{ii} = q_tva{ii}(1, :).'; % Transpose for later
+    cell_dq_human{ii} = q_tva{ii}(2, :).'; % Transpose for later
+    cell_ddq_human{ii} = q_tva{ii}(3, :).'; % Transpose for later 
+    cell_dddq_human{ii} = q_tva{ii}(4, :).'; % Transpose for later    
+end
+
+% Merge trajectories velocities and accelerations into one matrix
+q_human = [cell_q_human{:}].'; % Merge and transpose so rows correspond to single joint trajectories
+dq_human = [cell_dq_human{:}].'; % Merge and transpose so rows correspond to single joint velocities
+ddq_human = [cell_ddq_human{:}].'; % Merge and transpose so rows correspond to single joint accelerations
+dddq_human = [cell_dddq_human{:}].'; % Merge and transpose so rows correspond to single joint jerks
+
+%% Plot CoP
+
+% Get the external wrenches object
+EW_star = getExternalWrenches(q_star,L,LiftParam);
+EW_human = getExternalWrenches(q_human,L,LiftParam);
+
+% Get the COP
+COP_star = COP_6DOF_Matrix(q_star, dq_star, ddq_star, modelParam, EW_star);
+COP_human = COP_6DOF_Matrix(q_human, dq_human, ddq_human, modelParam, EW_human);
+
+if AppendixFlags.PlotCOP
+    % Plot the COP and the limits
+    figure;
+    hold on;
+    plot(Time, COP_star(1, :), 'DisplayName', 'COP-opt');
+    plot(Time, COP_human(1, :), 'DisplayName', 'COP-human');
+    plot(Time([1,end]), LiftParam.HeelPosition*ones(1,2), 'k--', 'DisplayName', 'COP-lb');
+    plot(Time([1,end]), LiftParam.ToePosition*ones(1,2), 'k--', 'DisplayName', 'COP-ub');
+    xlabel('Time [s]');
+    ylabel('Displacement [m]');
+    grid;
+    legend;
+    title('Comparison of human and optimization COP');
+end
--- src/Optimization/Motion_Optimization_Appendix_ItpResolutionCost.m
+++ src/Optimization/Motion_Optimization_Appendix_ItpResolutionCost.m
@@ -13,6 +13,44 @@ AppendixFlags.PowersSquared = false;
 AppendixFlags.VisualComparison = false;
 AppendixFlags.NumericalComparison = true;
 
+%% Get the time vector with the number of points desired and re-generate trajectories
+
+% Time
+Time = linspace(t0, tf, itpParam.ItpResolutionCost);
+
+% Extract knots of individual joints
+q_knots_star = cell(1, NJ);
+for ii = 1 : NJ
+    q_knots_star{ii} = x_star(1 + (ii-1) * Ncp : ii * Ncp);
+end
+
+% Get spline coeffs
+polycoeffs_star = cell(1, NJ);
+for ii = 1 : NJ
+    polycoeffs_star{ii}  = splineInterpolation2(Tknots, q_knots_star{ii}, ItpOrder, BndCnd);
+end
+
+% Get trajectories, velocities, accelerations, and jerks and separate them
+q_tva = cell(1, NJ);
+cell_q_star = cell(1, NJ);
+cell_dq_star = cell(1, NJ);
+cell_ddq_star = cell(1, NJ);
+cell_dddq_star = cell(1, NJ);
+
+for ii = 1 : NJ
+    q_tva{ii} = splineCoefToTrajectory(Tknots, polycoeffs_star{ii}, Time, 3);
+    cell_q_star{ii} = q_tva{ii}(1, :).'; % Transpose for later
+    cell_dq_star{ii} = q_tva{ii}(2, :).'; % Transpose for later
+    cell_ddq_star{ii} = q_tva{ii}(3, :).'; % Transpose for later 
+    cell_dddq_star{ii} = q_tva{ii}(4, :).'; % Transpose for later    
+end
+
+% Merge trajectories velocities and accelerations into one matrix
+q_star = [cell_q_star{:}].'; % Merge and transpose so rows correspond to single joint trajectories
+dq_star = [cell_dq_star{:}].'; % Merge and transpose so rows correspond to single joint velocities
+ddq_star = [cell_ddq_star{:}].'; % Merge and transpose so rows correspond to single joint accelerations
+dddq_star = [cell_dddq_star{:}].'; % Merge and transpose so rows correspond to single joint jerks
+
 %% Get human trajectories and compate with optimal ones
 
 % Human knots
@@ -91,11 +129,12 @@ end
 %% Calculate Torques for Human Trajectories and Compare with Optimal Trajectories
 
 % Get the external wrenches object
-EW = getExternalWrenches(q,L,LiftParam);
+EW_star = getExternalWrenches(q_star,L,LiftParam);
+EW_human = getExternalWrenches(q_human,L,LiftParam);
 
 % Calculate the dynamics for the optimal and human trajectories
-[GAMMA_star, EN_star] = Dyn_6DOF(q_star, dq_star, ddq_star, modelParam, EW);
-[GAMMA_human, EN_human] = Dyn_6DOF(q_human, dq_human, ddq_human, modelParam, EW);
+[GAMMA_star, EN_star] = Dyn_6DOF(q_star, dq_star, ddq_star, modelParam, EW_star);
+[GAMMA_human, EN_human] = Dyn_6DOF(q_human, dq_human, ddq_human, modelParam, EW_human);
 
 % Joint names
 Joints = {'Ankle', 'Knee', 'Hip', 'Back', 'Shoulder', 'Elbow'};

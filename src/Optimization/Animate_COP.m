e64bda1b8341c5944e1dcbe47f0e535560f89116
Filip <becaphilippe@gmail.com>
2021-06-23 17:12:09 +0200

    Replaced external wrench calculating code with a single function call

--- src/Optimization/Animate_COP.m
+++ src/Optimization/Animate_COP.m
@@ -1,41 +1,10 @@
 %ANIMATE_COP
 
-% Vertical gravitational force acting on the box COM, expressed in the 
-% robot base frame
-bF = [0; -LiftParam.BoxMass * LiftParam.Gravity; 0];
-
-% Moment of the gravitational force with respect to the wrist, expressed in
-% the robot base frame
-bM = cross(-LiftParam.BoxToWristVectorDuringLift, bF);
-
-% Get index of lift and drop off
-iLiftOff = floor(size(q, 2) * LiftParam.PercentageLiftOff);
-iDropOff = floor(size(q, 2) * LiftParam.PercentageDropOff);
-
-% Get transformation matrices of the wrist joint during the lifting motion
-T = FKM_nDOF_Cell(q(:, iLiftOff:iDropOff), L);
-Tw = T(end, :);
-
-% Get the force and moment of the gravitational force excerced on the wrist
-% expressed in the wrist frame
-wF = [];
-wM = [];
-
-for ii = 1 : length(Tw)
-    % Add the rotated force vector to the data structure
-    wF = [wF, Tw{ii}(1:3, 1:3)*bF];
-    % Add the rotated moment vector to the data structure
-    wM = [wM, Tw{ii}(1:3, 1:3)*bM];
-end
-
-% Get the zero external wrenches
-EW = zeroExternalWrenches6DOF(size(q, 2));
-
-% Modify the external wrenches at the end effector
-EW.EndEffectorWrenches = [zeros(6, iLiftOff-1), [wF;wM], zeros(6, size(q, 2) - iDropOff)];
+% Get external wrenches
+EW = getExternalWrenches(q, L, LiftParam);
 
 % Get COP
-COP = COP_6DOF_Matrix(q_star, dq_star, ddq_star, modelParam, zeroExternalWrenches6DOF(size(q_star, 2)));
+COP = COP_6DOF_Matrix(q_star, dq_star, ddq_star, modelParam, EW);
 % Plot COP
 opts.handleInits = {@()plot(0, 0, 'go', 'DisplayName', 'COP')};
 opts.callback = @(ii, handle) cop_callback(ii, handle, COP);
